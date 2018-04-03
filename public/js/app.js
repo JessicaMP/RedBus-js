96e2871f3fcb9929e5db926da071a7fd6e576aa5
JessicaMP <jessica95.mp@gmail.com>
2018-04-03 08:05:19 -0500

    Estilos completos en español

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,14 @@ const bancos = [{name: 'bcp', id: 1, img: 'assets/images/logo-bcp.svg', paso1: '
 ]
 
 const agentes = [
-    { name: 'bcp', id: 1, img: 'assets/images/logo-bcp.svg', info1: 'Agentes BCP: Brinda el código de empresa 02186', info2: 'Agencias BCP: Costo adicional: S/1.00' },
-    { name: 'bbva', id: 2, img: 'assets/images/logo-bbva.svg', info1: '', info2: '' },
-    { name: 'interbank', id: 3, img: 'assets/images/logo-interbank.svg', info1: 'Agentes Interbank: Brinda el código de empresa 2735001', info2: 'Agencias Market de Interbank: Costo adicional: S/2.00' },
-    { name: 'banbif', id: 4, img: 'assets/images/logo-banbif.svg', info1: '', info2: '' },
-    { name: 'fullcarga', id: 5, img: 'assets/images/logo-fullcarga.svg',info1: 'Encuentra a Full Carga en Bodegas, Farmacias, cabinas de internet y Locutorios', info2: '' },
-    { name: 'scotiabank', id: 6, img: 'assets/images/logo-scotiabank.svg', info1: '', info2: '' },
-    { name: 'westerUnion', id: 7, img: 'assets/images/logo-westerUnion.svg', info1: '', info2: '' },
-    { name: 'kasnet', id: 8, img: 'assets/images/logo-kasnet.svg', info1: '', info2: '' }
+    { name: 'bcp', id: 1, img: 'assets/images/logo-bcp.svg', att1: '(*)', info1: 'Agentes BCP: Brinda el código de empresa 02186', att2: '(*)', info2: 'Agencias BCP: Costo adicional: S/1.00' },
+    { name: 'bbva', id: 2, img: 'assets/images/logo-bbva.svg', att1: '', info1: '', att2: '', info2: '' },
+    { name: 'interbank', id: 3, img: 'assets/images/logo-interbank.svg', att1: '(*)', info1: 'Agentes Interbank: Brinda el código de empresa 2735001', att2: '(*)',info2: 'Agencias Market de Interbank: Costo adicional: S/2.00' },
+    { name: 'banbif', id: 4, img: 'assets/images/logo-banbif.svg', att1: '', info1: '', att2: '', info2: '' },
+    { name: 'fullcarga', id: 5, img: 'assets/images/logo-fullcarga.svg', att1: '(*)', info1: 'Encuentra a Full Carga en Bodegas, Farmacias, cabinas de internet y Locutorios', att2: '', info2: '' },
+    { name: 'scotiabank', id: 6, img: 'assets/images/logo-scotiabank.svg', att1: '', info1: '', att2: '', info2: '' },
+    { name: 'westerUnion', id: 7, img: 'assets/images/logo-westerUnion.svg', att1: '', info1: '', att2: '', info2: '' },
+    { name: 'kasnet', id: 8, img: 'assets/images/logo-kasnet.svg', att1: '', info1: '', att2: '', info2: '' }
 ]
   
 
@@ -22,9 +22,8 @@ const agentes = [
         $('img').click(function(){
             const $img = $(this);
             if ($img.attr('alt') == obj.name){
-                $('.datos').remove()
-                $('.pasos').append( `<p class=datos><img src="assets/images/one.svg" alt="Number One" class="numbers"> Ingresa tu codigo CIP:9125682 y sigue los pasos</p> <p class=datos> <img src="assets/images/two.svg" alt="Number Two" class="numbers"> ${obj.paso1}</p>` 
-                )
+                $('.datos').remove();
+                $('.pasos').append( `<p class=datos><img src="assets/images/one.svg" alt="Number One" class="numbers"> Ingresa tu codigo CIP:9125682 y sigue los pasos</p> <p class=datos> <img src="assets/images/two.svg" alt="Number Two" class="numbers"> ${obj.paso1}</p>`);
             }              
           });       
      });
@@ -35,8 +34,10 @@ const agentes = [
             const $img = $(this);
             if ($img.attr('alt') == obj.name){
                 $('.informacion').remove()
-                $('.info').append( `<p class="informacion">${obj.info1}</p> <p class="informacion">${obj.info2}</p>`)
-            }              
+                $('.info').html('');
+                const insert = ( `<p class="informacion col"><span class="c-orange">${obj.att1}</span> ${obj.info1}</p> <p class="informacion col text-right"><span class="c-orange">${obj.att2}</span> ${ obj.info2}</p>`)
+                $('.info').append(insert);
+              }              
           });       
      });
     
