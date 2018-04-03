5e568c2405c3afc3fe460e339d212f8ec9ccf42a
katherinelef <katherine.lef@hotmail.com>
2018-04-03 10:49:01 -0500

    detalles informacion

--- public/js/app-en.js
+++ public/js/app-en.js
@@ -6,14 +6,14 @@ const bancos = [{name: 'bcp', id: 1, img: 'assets/images/logo-bcp.svg', paso1: '
 ]
 
 const agentes = [
-    { name: 'bcp', id: 1, img: 'assets/images/logo-bcp.svg', info1: 'BCP Agents: Provide the company code 02186', info2: 'BCP agencies: Additional cost: S / 1.00' },
-    { name: 'bbva', id: 2, img: 'assets/images/logo-bbva.svg', info1: '', info2: '' },
-    { name: 'interbank', id: 3, img: 'assets/images/logo-interbank.svg', info1: 'Interbank Agents: Provides company code 2735001', info2: 'Interbank Market Agencies: Additional cost: S / 2.00' },
-    { name: 'banbif', id: 4, img: 'assets/images/logo-banbif.svg', info1: '', info2: '' },
-    { name: 'fullcarga', id: 5, img: 'assets/images/logo-fullcarga.svg',info1: 'You can find FullCarga in geocery stores, farmacies, internet cafés and parlors', info2: '' },
-    { name: 'scotiabank', id: 6, img: 'assets/images/logo-scotiabank.svg', info1: '', info2: '' },
-    { name: 'westerUnion', id: 7, img: 'assets/images/logo-westerUnion.svg', info1: '', info2: '' },
-    { name: 'kasnet', id: 8, img: 'assets/images/logo-kasnet.svg', info1: '', info2: '' }
+    { name: 'bcp', id: 1, att1: '(*)',img: 'assets/images/logo-bcp.svg', info1: 'BCP Agents: Provide the company code 02186', att2: '(*)',info2: 'BCP agencies: Additional cost: S / 1.00' },
+    { name: 'bbva', id: 2,  att1: '',img: 'assets/images/logo-bbva.svg', info1: '', att2: '',info2: '' },
+    { name: 'interbank', id: 3,att1: '(*)', img: 'assets/images/logo-interbank.svg', info1: 'Interbank Agents: Provides company code 2735001', att2: '(*)',info2: 'Interbank Market Agencies: Additional cost: S / 2.00' },
+    { name: 'banbif', id: 4, att1: '', img: 'assets/images/logo-banbif.svg', info1: '',att2: '', info2: '' },
+    { name: 'fullcarga', id: 5,att1: '(*)', img: 'assets/images/logo-fullcarga.svg',info1: 'You can find FullCarga in geocery stores, farmacies, internet cafés and parlors',att2: '',info2: '' },
+    { name: 'scotiabank', id: 6,  att1: '',img: 'assets/images/logo-scotiabank.svg', info1: '', att2: '', info2: '' },
+    { name: 'westerUnion', id: 7, att1: '', img: 'assets/images/logo-westerUnion.svg', info1: '', att2: '', info2: '' },
+    { name: 'kasnet', id: 8, att1: '', img: 'assets/images/logo-kasnet.svg', info1: '', att2: '', info2: '' }
 ]
   
 
@@ -35,7 +35,9 @@ const agentes = [
             const $img = $(this);
             if ($img.attr('alt') == obj.name){
                 $('.informacion').remove()
-                $('.info').append( `<p class="informacion">${obj.info1}</p> <p class="informacion">${obj.info2}</p>`)
+                $('.info').html('');
+                const insert = ( `<p class="informacion col-md col-12"><span class="c-orange">${obj.att1}</span> ${obj.info1}</p> <p class="informacion col-md col-12 pay-cash"><span class="c-orange">${obj.att2}</span> ${ obj.info2}</p>`)
+                $('.info').append(insert);
             }              
           });       
      });
